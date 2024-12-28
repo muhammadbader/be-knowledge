af499827246fea1958b82116fe43a7ae69c72234
mhmmd <muhammad@flare.watch>
2024-12-28 08:36:09 +0200

    status codes:

--- index.js
+++ index.js
@@ -21,17 +21,47 @@ app.use("/courses", courseRouter);
 
 app.post("/db/users", (req, res) => {
   const { name, email, passw } = req.body;
-
   const SQL = `INSERT INTO users(name, email, password) VALUES ('${name}', '${email}', '${passw}')`;
-  connection.execute(SQL);
+  connection.execute(SQL, (err, result) => {
+    if (err) {
+      // already exists
+      return res.status(409).json({ error: err });
+    }
+    return res.status(201).json({ message: "User created successfully" });
+  });
+});
 
-  return res.json({ message: "User created successfully" });
+app.delete("/db/users", (req, res) => {
+  const { id } = req.body;
+  const SQL = `DELETE FROM users WHERE id = ${id}`;
+  connection.execute(SQL, (err, resulst) => {
+    if (err || result.affectedRows === 0) {
+      // general client error (auth  ....)
+      return res.status(400).json({ error: err ? err : "User not found" });
+    }
+    return res.status(200).json({ message: "User deleted successfully" });
+  });
+});
+
+app.put("/db/users", (req, res) => {
+  const { id, name } = req.body;
+  const SQL = `UPDATE users SET name = '${name}' WHERE id = ${id}`;
+  connection.execute(SQL, (err, result) => {
+    if (err || result.affectedRows === 0) {
+      // not found error
+      return res.status(404).json({ error: err ? err : "User not found" });
+    }
+    return res.status(200).json({ message: "User updated successfully" });
+  });
 });
 
 app.get("db/users", (req, res) => {
   const SQL = "SELECT name, email, password FROM users";
   connection.execute(SQL, (err, result) => {
-    return result.json({ message: "OK", data: result });
+    if (err) {
+      return res.json({ error: err });
+    }
+    return res.status(200).json({ message: "OK", data: result });
   });
 });
 
